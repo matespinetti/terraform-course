7354fce629d4b907d34e3d6fa32dd2cc270ce2df
Mateo Spinetti <mateospinetti1@gmail.com>
2025-07-16 17:38:49 +0000

    init

---
+++ 02-Terraform-Basics/02-02-Terraform-Commands-Basics/terraform-manifests/ec2-instance.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2demo" {
+    ami = "ami-0150ccaf51ab55a51"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "ec2demo"
+    }
+}
