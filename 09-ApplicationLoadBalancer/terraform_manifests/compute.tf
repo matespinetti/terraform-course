869fb8f17da882831b0c26e7d132500b5cf635c0
Mateo Spinetti <mateospinetti1@gmail.com>
2025-07-19 00:00:54 +0000

    alb

---
+++ 09-ApplicationLoadBalancer/terraform_manifests/compute.tf
@@ -0,0 +1,49 @@
+#Bastion Host
+module "ec2_bastion_host" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "6.0.2"
+
+  name = "${local.name}-bastion-host"
+  ami = local.amazon_linux_2023_ami_id
+  instance_type = var.bastion_host_instance_type
+
+  #Network configuration
+  subnet_id = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+  create_eip = true #Create an Elastic IP for the bastion host
+
+  #Security and access
+  key_name = var.bastion_host_key_pair_name
+  tags = local.common_tags
+
+
+
+
+}
+
+module "ec2_private_app_server" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "6.0.2"
+
+  for_each = local.private_instances
+
+  name = each.value.instance_name
+  ami = local.amazon_linux_2023_ami_id
+  instance_type = var.private_app_instance_type
+
+  #Network configuration
+  subnet_id = each.value.subnet_id
+  vpc_security_group_ids = [module.private_app_sg.security_group_id]
+  associate_public_ip_address = false
+
+
+  #Security and access
+  key_name = var.private_app_instance_key_pair_name
+
+  #User data
+  user_data = file("${path.module}/app1-install.sh")
+
+  tags = local.common_tags
+
+  depends_on = [ module.vpc ]
+}
---
+++ 09-ApplicationLoadBalancer/terraform_manifests/security-group.tf
@@ -0,0 +1,90 @@
+#Public Bastion Host Security Group
+module "public_bastion_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+
+  name = "${local.name}-public-bastion-sg"
+  description = "Security Group with SSH port open from everybody, egress ports are all open"
+  vpc_id = module.vpc.vpc_id
+  #Ingress rules and CIDR blocks
+  ingress_with_cidr_blocks = [
+    {
+        rule = "ssh-tcp"
+        cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  #Egress rules and CIDR blocks
+  egress_with_cidr_blocks = [
+    {
+        rule = "all-all"
+        cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = local.common_tags
+}
+
+
+#Private App Server Security Grup
+module "private_app_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+
+  name = "${local.name}-private-app-sg"
+  description = "Security Group with HTTP and SSH port open from public bastion host, egress ports are all open"
+  vpc_id = module.vpc.vpc_id
+
+
+  computed_ingress_with_source_security_group_id = [
+    {
+        rule = "ssh-tcp"
+        source_security_group_id = module.public_bastion_sg.security_group_id
+    },
+    {
+        rule = "http-80-tcp"
+        source_security_group_id = module.alb_sg.security_group_id
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+        rule = "all-all"
+        cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  number_of_computed_ingress_with_source_security_group_id = 2
+
+  tags = local.common_tags
+
+  
+}
+
+
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+
+
+  name = "${local.name}-alb-sg"
+  description = "Security Group with HTTP and HTTPS port open from the internet"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      rule = "http-80-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      rule = "https-443-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      rule = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = local.common_tags
+}
